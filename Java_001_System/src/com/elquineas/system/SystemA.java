f98bcc6dcd5b37e581d7500ad71f9f85b09334b4
elquineas <elquineas@naver.com>
2023-04-14 10:44:54 +0900

    System_클래스

---
+++ Java_001_System/src/com/elquineas/system/SystemA.java
@@ -0,0 +1,18 @@
+package com.elquineas.system;
+
+// class 이름은 반드시 첫글자 대문자
+// 2개 이상의 단어를 조합하는 것을 권장
+// Camel case로 작성한다
+public class SystemA {
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		System.err.println("반갑습니다");
+		System.out.println();
+		System.out.print(30);
+		System.out.print("+");
+		System.out.print(40);
+		System.out.print("=");
+		System.out.print(30+40);
+		System.out.println("안녕하세요");
+	}
+}
